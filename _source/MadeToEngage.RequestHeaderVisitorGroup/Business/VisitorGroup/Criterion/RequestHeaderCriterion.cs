5942af94603cef4127ec12e95a053e8794f6caff
Johnny <jonathan.mullaney@gmail.com>
2020-05-05 17:53:45 +0100

    initial commit

---
+++ _source/MadeToEngage.RequestHeaderVisitorGroup/Business/VisitorGroup/Criterion/RequestHeaderCriterion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using EPiServer.Personalization.VisitorGroups;
+using MadeToEngage.RequestHeaderVisitorGroup.Business.VisitorGroup.Model;
+
+namespace MadeToEngage.RequestHeaderVisitorGroup.Business.VisitorGroup.Criterion
+{
+    [VisitorGroupCriterion(
+        Category = "Request Criteria",
+        Description = "Matches the request to see if it contains a header / with the specified value",
+        DisplayName = "Request Header")]
+    public class RequestHeaderCriterion : CriterionBase<RequestHeaderModel>
+    {
+        public override bool IsMatch(IPrincipal principal, HttpContextBase httpContext)
+        {
+            NameValueCollection requestHeaders = httpContext.Request.Headers;
+
+            if (Model.Condition == RequestHeaderValueCondition.Exists)
+                return requestHeaders.AllKeys.Contains(Model.Key);
+
+            string keyValue = requestHeaders[Model.Key];
+
+            return !string.IsNullOrEmpty(keyValue)
+                   && string.Equals(keyValue, Model.Value, StringComparison.CurrentCultureIgnoreCase);
+        }
+    }
+}
---
+++ _source/MadeToEngage.RequestHeaderVisitorGroup/Business/VisitorGroup/Model/RequestHeaderModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Personalization.VisitorGroups;
+using EPiServer.Web.Mvc.VisitorGroups;
+
+namespace MadeToEngage.RequestHeaderVisitorGroup.Business.VisitorGroup.Model
+{
+    public class RequestHeaderModel : CriterionModelBase
+    {
+        [Required]
+        public string Key { get; set; }
+
+        [Required, DojoWidget(SelectionFactoryType = typeof(EnumSelectionFactory))]
+        public RequestHeaderValueCondition Condition { get; set; }
+        public string Value { get; set; }
+
+        public override ICriterionModel Copy()
+        {
+            return base.ShallowCopy();
+        }
+    }
+
+    public enum RequestHeaderValueCondition
+    {
+        Exists,
+        HasValue
+    }
+}
